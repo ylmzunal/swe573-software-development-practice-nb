34bcbfbda58313aa5fee64c0aabb0748753f1578
betulnesibe <80793176+betulnesibe@users.noreply.github.com>
2024-10-27 23:03:47 +0300

    CourseApp is initialized for the course.

--- btk_djangoCourse/CourseApp/CourseApp/settings.py
+++ btk_djangoCourse/CourseApp/CourseApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "courses", # to add other apps inside the app
 ]
 
 MIDDLEWARE = [
