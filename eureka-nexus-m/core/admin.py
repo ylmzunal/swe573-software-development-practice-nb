1196c8341137497beba7538f42ade8df57235c1f
betulnesibe <80793176+betulnesibe@users.noreply.github.com>
2024-12-16 03:10:01 +0300

    admin panel is modified

--- eureka-nexus-m/core/admin.py
+++ eureka-nexus-m/core/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Profile
+from .models import Profile, Post, PostFollower, WikidataTag, PostAttribute, PostMultimedia, Comment, Vote, UserFollower
 from .forms import ProfileCreationForm, ProfileChangeForm
 
 class ProfileAdmin(UserAdmin):
-    add_form = ProfileCreationForm
-    form = ProfileChangeForm
     model = Profile
-    list_display = ['username', 'email', 'first_name', 'last_name']
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'bio', 'birthday')
+    fieldsets = UserAdmin.fieldsets + (  # Add custom fields to the admin interface
+        ('Additional Info', {
+            'fields': ('profile_picture', 'bio', 'birthday'),
+        }),
+    )
 
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Post)
+admin.site.register(PostFollower)
+admin.site.register(WikidataTag)
+admin.site.register(PostAttribute)
+admin.site.register(PostMultimedia)
+admin.site.register(Comment)
+admin.site.register(Vote)
+admin.site.register(UserFollower)
